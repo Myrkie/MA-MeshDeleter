815dcef8f948d0660306d3dcdfec2f725f4d4310
Myrkur <Myrkur@Myrkur.dev>
2025-02-01 01:34:46 -0500

    bumped version, fix missing inheritance

--- Runtime/SkinnedMeshBlendShapeRemoverBehavior.cs
+++ Runtime/SkinnedMeshBlendShapeRemoverBehavior.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
+using VRC.SDKBase;
 
 namespace myrkur.dev.ndmf.runtime
 {
-    public class SkinnedMeshBlendShapeRemoverBehavior : MonoBehaviour
+    public class SkinnedMeshBlendShapeRemoverBehavior : MonoBehaviour, IEditorOnly
     {
         public string blendShapeName;
     }
